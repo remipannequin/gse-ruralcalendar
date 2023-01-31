ad27596afd4bbfd32e3def33fc352f9590af111d
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2023-01-31 15:51:03 +0100

    use wikimedia API

--- get_wiki_links.py
+++ get_wiki_links.py
@@ -1,30 +1,32 @@
 
 
-import urllib.request
+import requests
 import re
 import json
 
+
+# First kind of page: one page, each trimester has it own section
 titles = [
-    ('fr', 'Calendrier_r%C3%A9publicain', [29, 30, 31, 32]),
-    ('en', 'French_Republican_calendar', [10, 11, 12, 13]),
-    ('es', 'Calendario_republicano_francés', [9, 10, 11, 12]),
-    ()
+    ('fr', 'Calendrier_r%C3%A9publicain', [29, 30, 31, 32], ".*FrRepCalLine.*\\[\\[(.*)\\]\\].*"),
+    ('en', 'French_Republican_calendar', [10, 11, 12, 13],".*FrRepCalLine.*\\[\\[(.*)\\]\\].*" ),
+    ('es', 'Calendario_republicano_franc%C3%A9s', [9, 10, 11, 12], "#\\W*\\'\\'.*\\'\\'\\W*\\(\\[\\[(.*)\\]\\]\\)")
     ]
 
 pattern = re.compile(".*FrRepCalLine.*\\[\\[(.*)\\]\\].*")
 
-for (lang, title, sections) in titles:
+for (lang, title, sections, pattern_str) in titles:
+    pattern = re.compile(pattern_str)
     saints = []
     for s in sections:
         # Wiki URL
-        wikiUrl = f'https://{lang}.wikipedia.org/w/index.php?title={title}&action=edit&section={s}'
+        wikiUrl = f'https://{lang}.wikipedia.org/w/api.php?page={title}&action=parse&section={s}&prop=wikitext&format=json'
 
-        rsp = urllib.request.urlopen(wikiUrl)
-        if not rsp.getcode() == 200:
-            # Warning, Not found
-            break
-        for line in rsp.readlines():
-            match = pattern.match(line.decode("utf-8"))
+        rsp = requests.get(wikiUrl)
+        rsp.raise_for_status()
+        data = rsp.json()
+        content = data['parse']['wikitext']['*']
+        for line in content.split('\n'):
+            match = pattern.match(line)
             if match:
                 found = match.group(1)
                 if '|' in found:
@@ -39,3 +41,4 @@ for (lang, title, sections) in titles:
 
     with open(f'french-republican-calendar_{lang}.json', 'w') as f:
         json.dump(saints, f)
+
