f7fc4ab287ad9a719e50643f72c1fd2a249df767
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2023-02-01 09:36:48 +0100

    remove some lint warning

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* extension.js
  *
  * This program is free software: you can redistribute it and/or modify
@@ -35,7 +36,7 @@ const _ = Domain.gettext;
 // translator should make sure that such file exists...
 let lang;
 
-// Code from extension FRC@jcdubacq.dubacq.fr
+// Bellow is some code from gnome shell extension FRC@jcdubacq.dubacq.fr
 
 // Astro code lifted from https://www.fourmilab.ch/documents/calendar/
 // The copyright there states this code belongs to the public domain.
@@ -94,28 +95,28 @@ Astro.prototype = {
         return a - (b * Math.floor(a / b));
     },
 
-    /*  DELTAT  --  Determine the difference, in seconds, between
-        Dynamical time and Universal time.  */
-    /*  Table of observed Delta T values at the beginning of
-        even numbered years from 1620 through 2002.  */
-    deltaTtab: [
-        121, 112, 103, 95, 88, 82, 77, 72, 68, 63, 60, 56, 53, 51, 48, 46,
-        44, 42, 40, 38, 35, 33, 31, 29, 26, 24, 22, 20, 18, 16, 14, 12,
-        11, 10, 9, 8, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10,
-        10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13,
-        13, 14, 14, 14, 14, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16,
-        16, 16, 15, 15, 14, 13, 13.1, 12.5, 12.2, 12, 12, 12, 12, 12, 12,
-        11.9, 11.6, 11, 10.2, 9.2, 8.2, 7.1, 6.2, 5.6, 5.4, 5.3, 5.4, 5.6,
-        5.9, 6.2, 6.5, 6.8, 7.1, 7.3, 7.5, 7.6, 7.7, 7.3, 6.2, 5.2, 2.7,
-        1.4, -1.2, -2.8, -3.8, -4.8, -5.5, -5.3, -5.6, -5.7, -5.9, -6,
-        -6.3, -6.5, -6.2, -4.7, -2.8, -0.1, 2.6, 5.3, 7.7, 10.4, 13.3, 16,
-        18.2, 20.2, 21.1, 22.4, 23.5, 23.8, 24.3, 24, 23.9, 23.9, 23.7,
-        24, 24.3, 25.3, 26.2, 27.3, 28.2, 29.1, 30, 30.7, 31.4, 32.2,
-        33.1, 34, 35, 36.5, 38.3, 40.2, 42.2, 44.5, 46.5, 48.5, 50.5,
-        52.2, 53.8, 54.9, 55.8, 56.9, 58.3, 60, 61.6, 63, 65, 66.6,
-    ],
     deltat(year) {
         var dt, f, i, t;
+        /*  DELTAT  --  Determine the difference, in seconds, between
+        Dynamical time and Universal time.  */
+        /*  Table of observed Delta T values at the beginning of
+        even numbered years from 1620 through 2002.  */
+        var deltaTtab = [
+            121, 112, 103, 95, 88, 82, 77, 72, 68, 63, 60, 56, 53, 51, 48, 46,
+            44, 42, 40, 38, 35, 33, 31, 29, 26, 24, 22, 20, 18, 16, 14, 12,
+            11, 10, 9, 8, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10,
+            10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13,
+            13, 14, 14, 14, 14, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16,
+            16, 16, 15, 15, 14, 13, 13.1, 12.5, 12.2, 12, 12, 12, 12, 12, 12,
+            11.9, 11.6, 11, 10.2, 9.2, 8.2, 7.1, 6.2, 5.6, 5.4, 5.3, 5.4, 5.6,
+            5.9, 6.2, 6.5, 6.8, 7.1, 7.3, 7.5, 7.6, 7.7, 7.3, 6.2, 5.2, 2.7,
+            1.4, -1.2, -2.8, -3.8, -4.8, -5.5, -5.3, -5.6, -5.7, -5.9, -6,
+            -6.3, -6.5, -6.2, -4.7, -2.8, -0.1, 2.6, 5.3, 7.7, 10.4, 13.3, 16,
+            18.2, 20.2, 21.1, 22.4, 23.5, 23.8, 24.3, 24, 23.9, 23.9, 23.7,
+            24, 24.3, 25.3, 26.2, 27.3, 28.2, 29.1, 30, 30.7, 31.4, 32.2,
+            33.1, 34, 35, 36.5, 38.3, 40.2, 42.2, 44.5, 46.5, 48.5, 50.5,
+            52.2, 53.8, 54.9, 55.8, 56.9, 58.3, 60, 61.6, 63, 65, 66.6,
+        ];
         if ((year >= 1620) && (year <= 2000)) {
             i = Math.floor((year - 1620) / 2);
             f = ((year - 1620) / 2) - i;  /* Fractional part of year */
@@ -495,6 +496,12 @@ class RuralCalendar {
         this.update(new Date());
     }
 
+    /**
+     * Transform a number into a string in the roman numbering system.
+     *
+     * @param {int} n the year to format
+     * @returns a string representing n in roman numbering
+     */
     romanNumeral(n) {
         var val, s = '', limit = 3999, i = 0;
         var v = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -711,8 +718,12 @@ class RuralCalendarTopMenu extends PanelMenu.Button {
      * Fetch the details from wikipedia.
      */
     fetch_detail() {
-        if (!this.detail_url)
-            log("Invalid URL, won't fetch details.");
+        if (!this.detail_url) {
+            // no detail provided for this day, hide detail panel
+            this.detail_menu_item.hide();
+        }
+        this.detail_menu_item.show();
+
 
         // TODO: what happen if there is no network connection ?
         let detailFile = Gio.File.new_for_uri(this.detail_url);
